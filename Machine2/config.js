25f1898722cd9bf37220f097303c9aa3f2c1b76d
HassanTofayli <85042863+HassanTofayli@users.noreply.github.com>
2023-04-22 14:49:38 +0300

    Add files via upload

---
+++ Machine2/config.js
@@ -0,0 +1,166 @@
+
+window.Yofla360_configs = window.Yofla360_configs || {};
+window.Yofla360_configs['LG2F0iTrG'] = {
+    "custom": {},
+    "hotspots": {},
+    "settings": {
+        "rotation": {
+            "rotatePeriod": 2.5,
+            "rotateOnStart": true,
+            "rotateOnce": true,
+            "rotateDirection": 1,
+            "bounce": false
+        },
+        "preloader": {
+            "image": 0,
+            "showStartButton": false,
+            "startImage": "",
+            "startImageSizing": "fit",
+            "startImageBgColor": {
+                "r": 0,
+                "g": 0,
+                "b": 0,
+                "a": 0.5
+            },
+            "placeholderImage": "imagesOriginal/1.jpg"
+        },
+        "userInterface": {
+            "showToggleFullscreenButton": true,
+            "showTogglePlayButton": true,
+            "showArrows": true
+        },
+        "control": {
+            "reverseDrag": false
+        }
+    },
+    "images": [
+        {
+            "src": "1.jpg"
+        },
+        {
+            "src": "2.jpg"
+        },
+        {
+            "src": "3.jpg"
+        },
+        {
+            "src": "4.jpg"
+        },
+        {
+            "src": "5.jpg"
+        },
+        {
+            "src": "6.jpg"
+        },
+        {
+            "src": "7.jpg"
+        },
+        {
+            "src": "8.jpg"
+        },
+        {
+            "src": "9.jpg"
+        },
+        {
+            "src": "10.jpg"
+        },
+        {
+            "src": "11.jpg"
+        },
+        {
+            "src": "12.jpg"
+        },
+        {
+            "src": "13.jpg"
+        },
+        {
+            "src": "14.jpg"
+        },
+        {
+            "src": "15.jpg"
+        },
+        {
+            "src": "16.jpg"
+        },
+        {
+            "src": "17.jpg"
+        },
+        {
+            "src": "18.jpg"
+        },
+        {
+            "src": "19.jpg"
+        },
+        {
+            "src": "20.jpg"
+        },
+        {
+            "src": "21.jpg"
+        },
+        {
+            "src": "22.jpg"
+        },
+        {
+            "src": "23.jpg"
+        },
+        {
+            "src": "24.jpg"
+        },
+        {
+            "src": "25.jpg"
+        },
+        {
+            "src": "26.jpg"
+        },
+        {
+            "src": "27.jpg"
+        },
+        {
+            "src": "28.jpg"
+        },
+        {
+            "src": "29.jpg"
+        },
+        {
+            "src": "30.jpg"
+        },
+        {
+            "src": "31.jpg"
+        },
+        {
+            "src": "32.jpg"
+        },
+        {
+            "src": "33.jpg"
+        },
+        {
+            "src": "34.jpg"
+        },
+        {
+            "src": "35.jpg"
+        },
+        {
+            "src": "36.jpg"
+        }
+    ],
+    "meta": {
+        "images": {
+            "maxWidth": 498,
+            "ratio": 1.9840637450199203,
+            "folderNames": {
+                "360": "images360",
+                "1080": "images1080",
+                "original": "imagesOriginal"
+            }
+        },
+        "creator": {
+            "buildId": 121,
+            "playerBuildId": 52,
+            "createdAt": "2023-04-22T09:15:37.864Z",
+            "updatedAt": "2023-04-22T11:45:13.688Z",
+            "latestVersionCode": "va"
+        }
+    }
+};
+window.Yofla360_configs['yofla360_Machine2'] = window.Yofla360_configs['LG2F0iTrG']
+window.RotationData = window.Yofla360_configs['LG2F0iTrG']
