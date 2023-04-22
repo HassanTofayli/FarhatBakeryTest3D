66ce3720eb093e6df27736d3cd64488a95a11d78
HassanTofayli <85042863+HassanTofayli@users.noreply.github.com>
2023-04-22 14:47:00 +0300

    Add files via upload

---
+++ Machine1/config.js
@@ -0,0 +1,166 @@
+
+window.Yofla360_configs = window.Yofla360_configs || {};
+window.Yofla360_configs['zmApxHGm1'] = {
+    "custom": {},
+    "hotspots": {},
+    "settings": {
+        "rotation": {
+            "rotatePeriod": 2.5,
+            "rotateOnStart": true,
+            "rotateOnce": true,
+            "rotateDirection": 1,
+            "bounce": false
+        },
+        "preloader": {
+            "image": 0,
+            "showStartButton": false,
+            "startImage": "",
+            "startImageSizing": "fit",
+            "startImageBgColor": {
+                "r": 0,
+                "g": 0,
+                "b": 0,
+                "a": 0.5
+            },
+            "placeholderImage": "imagesOriginal/frame.0new .jpg"
+        },
+        "userInterface": {
+            "showToggleFullscreenButton": true,
+            "showTogglePlayButton": true,
+            "showArrows": true
+        },
+        "control": {
+            "reverseDrag": false
+        }
+    },
+    "images": [
+        {
+            "src": "frame.0new_.jpg"
+        },
+        {
+            "src": "frame.1new_.jpg"
+        },
+        {
+            "src": "frame.2new_.jpg"
+        },
+        {
+            "src": "frame.3new_.jpg"
+        },
+        {
+            "src": "frame.4new_.jpg"
+        },
+        {
+            "src": "frame.5new_.jpg"
+        },
+        {
+            "src": "frame.6new_.jpg"
+        },
+        {
+            "src": "frame.7new_.jpg"
+        },
+        {
+            "src": "frame.8new_.jpg"
+        },
+        {
+            "src": "frame.9new_.jpg"
+        },
+        {
+            "src": "frame.10new_.jpg"
+        },
+        {
+            "src": "frame.11new_.jpg"
+        },
+        {
+            "src": "frame.12new_.jpg"
+        },
+        {
+            "src": "frame.13new_.jpg"
+        },
+        {
+            "src": "frame.14new_.jpg"
+        },
+        {
+            "src": "frame.15new_.jpg"
+        },
+        {
+            "src": "frame.16new_.jpg"
+        },
+        {
+            "src": "frame.17new_.jpg"
+        },
+        {
+            "src": "frame.18new_.jpg"
+        },
+        {
+            "src": "frame.19new_.jpg"
+        },
+        {
+            "src": "frame.20new_.jpg"
+        },
+        {
+            "src": "frame.21new_.jpg"
+        },
+        {
+            "src": "frame.22new_.jpg"
+        },
+        {
+            "src": "frame.23new_.jpg"
+        },
+        {
+            "src": "frame.24new_.jpg"
+        },
+        {
+            "src": "frame.25new_.jpg"
+        },
+        {
+            "src": "frame.26new_.jpg"
+        },
+        {
+            "src": "frame.27new_.jpg"
+        },
+        {
+            "src": "frame.28new_.jpg"
+        },
+        {
+            "src": "frame.29new_.jpg"
+        },
+        {
+            "src": "frame.30new_.jpg"
+        },
+        {
+            "src": "frame.31new_.jpg"
+        },
+        {
+            "src": "frame.32new_.jpg"
+        },
+        {
+            "src": "frame.33new_.jpg"
+        },
+        {
+            "src": "frame.34new_.jpg"
+        },
+        {
+            "src": "frame.35new_.jpg"
+        }
+    ],
+    "meta": {
+        "images": {
+            "maxWidth": 723,
+            "ratio": 1.9862637362637363,
+            "folderNames": {
+                "360": "images360",
+                "1080": "images1080",
+                "original": "imagesOriginal"
+            }
+        },
+        "creator": {
+            "buildId": 121,
+            "playerBuildId": 52,
+            "createdAt": "2023-04-22T09:15:37.864Z",
+            "updatedAt": "2023-04-22T09:54:53.455Z",
+            "latestVersionCode": "va"
+        }
+    }
+};
+window.Yofla360_configs['yofla360_Untitled_project'] = window.Yofla360_configs['zmApxHGm1']
+window.RotationData = window.Yofla360_configs['zmApxHGm1']
